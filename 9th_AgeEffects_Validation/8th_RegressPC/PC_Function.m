ecbaf4598ef45f9fa3ee75208e8c43e589c874ec
Zaixu Cui <zaixucui@gmail.com>
2020-03-13 06:18:22 -0400

    codes

--- 9th_AgeEffects_Validation/8th_RegressPC/PC_Function.m
+++
@@ -1,8 +0,0 @@
-
-function PC_Calculation(ConnMatrix, Community_Vector, scanID, ResultantFolder)
-
-PC = participation_coef(ConnMatrix, Community_Vector);
-save([ResultantFolder '/' num2str(scanID) '.mat'], 'PC', 'scanID');
-
-
-
